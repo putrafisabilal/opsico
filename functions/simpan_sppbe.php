ccc987216ce4fa7e8a0f611229fd91d783843de5
Bbalqis <bbalqis@ymail.com>
2018-04-27 21:26:23 +0700

    simpan sppbe

--- functions/simpan_sppbe.php
+++ functions/simpan_sppbe.php
@@ -6,17 +6,18 @@
     $thrt = $_POST['thrt'];
     $t = $_POST['t'];
 
-  $cek = "SELECT nospa FROM sppbe where nospa='$nospa'"
-  $cekcek = $con->query($query);
-
-  if($cekcek){
+  $cek = "SELECT nospa FROM sppbe where nospa='$nospa'";
+  $cekcek = $con->query($cek);
+  $row = $cekcek->fetch_object();
+  $v = $row->nospa;
+  if(empty($v)){
 
       $query = "INSERT INTO sppbe(sppbe,nospa,thruput_harian,thruput_total,type) VALUES ('".$sppbe."','".$nospa."','".$thrh."','".$thrt."','".$t."');";
       // echo die($query);
       $tambahdata = $con->query($query);
 
       if ($tambahdata) {
-        header("location:../transaksi/form-sppbe.php?save");
+        header("location:../transaksi/form-sppbe.php?status=1);
       } else {
         header('location:../transaksi/form-sppbe.php?status=4');
       }
